4e517d319d8ddb3093223b3671d61a8648ddcd89
badalsaibo <badalsaibo@gmail.com>
2023-03-23 13:23:08 +0530

    feat: with mantine

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,32 +1,28 @@
 import type { AppProps } from 'next/app';
 
-import { CssVarsProvider, extendTheme } from '@mui/joy/styles';
 import themeConfig from 'config/theme';
-import GlobalStyles from '@mui/joy/GlobalStyles';
-import CssBaseline from '@mui/joy/CssBaseline';
 import Layout from 'components/Layout';
 import HydrationResolver from 'components/HydrationResolver';
+import { MantineProvider } from '@mantine/core';
 
-const globalStyles = (
-  <GlobalStyles
-    styles={{
-      'html, body, div#__next': {
-        height: '100%',
-      },
-    }}
-  />
-);
+// const globalStyles = (
+//   <GlobalStyles
+//     styles={{
+//       'html, body, div#__next': {
+//         height: '100%',
+//       },
+//     }}
+//   />
+// );
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <CssVarsProvider theme={extendTheme(themeConfig)} defaultMode="system">
-      {globalStyles}
+    <MantineProvider withGlobalStyles withNormalizeCSS theme={themeConfig}>
       <HydrationResolver>
         <Layout>
-          <CssBaseline />
           <Component {...pageProps} />
         </Layout>
       </HydrationResolver>
-    </CssVarsProvider>
+    </MantineProvider>
   );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,15 +1,20 @@
-import { getInitColorSchemeScript } from '@mui/joy/styles';
+import { createGetInitialProps } from '@mantine/next';
 import { Html, Head, Main, NextScript } from 'next/document';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        {getInitColorSchemeScript()}
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+const getInitialProps = createGetInitialProps();
+
+export default class _Document extends Document {
+  static getInitialProps = getInitialProps;
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
